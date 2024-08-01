0c6bdd04ae033cf0561431a27102df14c1f29c92
Narendra <nskathayat77@gmail.com>
2024-08-01 21:20:41 +0530

    cors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-require('dotenv').config()
+require('dotenv').config();
 const port = 4000;
 const cors = require("cors");
 const connectDb = require("./db");
@@ -8,28 +8,24 @@ const app = express();
 
 connectDb();
 
-
 app.use(cookieParser());
-app.use(express.json()); // Using middleware(If i have to use req.body then i have to write this middleware)
+app.use(express.json()); // Using middleware(If I have to use req.body then I have to write this middleware)
 app.use("/Multer", express.static("Multer"));
-
 
 app.use(
     cors({
         origin: ['http://localhost:5173', 'http://localhost:5174'], // Allow requests from these origins
-        withCredentials: true, // Allow cookies to be sent with the requests
+        credentials: true, // Allow cookies to be sent with the requests
     })
 );
-
-// app.use(cors())
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/form', require('./routes/form'));
 app.use('/api/admin_auth', require('./routes/admin_auth'));
 app.use('/api/gallery', require('./routes/AddGallary'));
-app.use('/api/category',require('./routes/AddCategory'));
-app.use('/api/product',require('./routes/AddProducts'));
-app.use('/api/orders',require('./routes/Orders'));
+app.use('/api/category', require('./routes/AddCategory'));
+app.use('/api/product', require('./routes/AddProducts'));
+app.use('/api/orders', require('./routes/Orders'));
 app.use('/api/coupon', require('./routes/Coupon_Dis'));
 
 app.listen(port, () => {
